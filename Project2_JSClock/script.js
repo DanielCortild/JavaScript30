333734738461ecb9bf3dd32ae92b43cfce4fce05
DanielCortild <daniel@cortild.dk>
2020-03-16 21:42:28 +0100

    Project 2

---
+++ Project2_JSClock/script.js
@@ -0,0 +1,20 @@
+const HourHand = document.querySelector('#HourHand');
+
+function setTime() {
+  var time = new Date();
+
+  const SecondHand = document.querySelector('#SecondHand');
+  var seconds = time.getSeconds() * 6;
+  SecondHand.style.transform = `translateX(3px) rotate(${seconds}deg)`;
+  console.log(seconds);
+
+  const MinuteHand = document.querySelector('#MinuteHand');
+  var minutes = time.getMinutes() * 6;
+  MinuteHand.style.transform = `translateX(3px) rotate(${minutes}deg)`;
+
+  const HourHand = document.querySelector('#HourHand');
+  var hours = time.getHours() * 6;
+  HourHand.style.transform = `translateX(3px) rotate(${hours}deg)`;
+}
+
+setInterval(setTime, 1000);
